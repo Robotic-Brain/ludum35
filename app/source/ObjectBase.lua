501f6bdb89d0ca106f6207c60d2243a021c62e61
Robotic-Brain <github@roboticbrain.de>
2016-04-16 11:32:00 +0200

    Adding subclass name to tostring method

--- app/source/ObjectBase.lua
+++ app/source/ObjectBase.lua
@@ -1,10 +1,27 @@
 Object = {}
 
-function Object:new (o)
+local function my_tostring(o)
+    local mt = getmetatable(o)
+    local tmp = mt.__tostring
+    mt.__tostring = nil
+    local res = (mt.__class or "Object")..": "..tostring(o)
+    mt.__tostring = tmp
+    return res
+end
+
+function Object:new(o)
     o = o or {}
     setmetatable(o, self)
     self.__index = self
+    self.__tostring = my_tostring
     return o
+end
+
+function subclass(name, base, staticfields)
+    assert(type(name) == 'string', "No Classname given!")
+    local c = base:new(staticfields)
+    getmetatable(c).__class = name
+    return c
 end
 
 function Object.instanceof(subject, super)
